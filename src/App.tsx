afc822929622029fb94b45dd120b71783c1e0efa
Klaidas Bespalovas <klaidaz01@gmail.com>
2021-11-20 13:34:50 +0200

    add chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React, { useContext } from "react";
+import { Box } from "@chakra-ui/layout";
 
 import "./App.css";
 import DataContext from "./context/DataContext";
@@ -8,9 +10,16 @@ function App() {
   const data = useContext(DataContext);
 
   return (
-    <div className="App">
-      <TotalUsageByDeviceChart data={data} />
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <TotalUsageByDeviceChart data={data} />
+        <Box
+          w="100%"
+          h="200px"
+          bgGradient="linear(to-r, green.200, pink.500)"
+        />
+      </div>
+    </ChakraProvider>
   );
 }
 
