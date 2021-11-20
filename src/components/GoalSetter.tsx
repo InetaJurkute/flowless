62a55da9bed1d83a498259597c2e317abf04e9b9
Ineta Jurkutė <ineta.jurkute@devbridge.com>
2021-11-20 18:35:58 +0200

    Update goal setter

--- src/components/GoalSetter.tsx
+++ src/components/GoalSetter.tsx
@@ -1,6 +1,13 @@
 import { Button } from "@chakra-ui/button";
-import { DataSet } from "../context/DataContext";
+import {
+  Appliance,
+  currentDate,
+  currentMonth,
+  DataSet,
+  Measurement,
+} from "../context/DataContext";
 import { Form, Formik, Field, ErrorMessage } from "formik";
+import { sumBy, sum } from "lodash";
 
 enum GoalType {
   Liters = "Liters",
@@ -12,11 +19,65 @@ interface MonthlyGoal {
   monthlyGoalAmount: string;
 }
 
+//In Helsinki, residents pay 1.96 euros per cubic metre of clean drinking water.
+const waterLiterPrice = 1.96 / 1000;
+
+// Finland Household, kWh price: 0.160 EURO
+const electricityPrice = 0.16;
+
+//money needed by liters = liters * price + heating power * price
+//liters needed by money = average heating needed per last 12 months * price + average liters needed per last 12 months * price
+
+const getAverageHeatingForLiterOfWater = (
+  data: DataSet,
+  months: number //use data from last N months
+) => {
+  const myApartmentData = data.houses[0].apartments[0];
+  const sumByDevice = Object.values(Appliance).map((appliance) => {
+    const myMeasurements = myApartmentData[appliance].measurements.filter(
+      (m) =>
+        m.TimeStamp >= currentDate.minus({ months }).toString() &&
+        m.TimeStamp <= currentDate.toString()
+    );
+
+    return {
+      liters: sumBy(myMeasurements, (x) =>
+        parseFloat((x as Measurement).Consumption)
+      ),
+      power: sumBy(myMeasurements, (x) =>
+        parseFloat((x as Measurement).Power_Consumption)
+      ),
+    };
+  });
+
+  const totalPower = sumBy(sumByDevice, (x) => x.power);
+  const totalLiters = sumBy(sumByDevice, (x) => x.liters);
+
+  return totalPower / totalLiters;
+};
+
 export const GoalSetter = ({ data }: { data: DataSet }) => {
   const handleSubmit = (values: MonthlyGoal, {}) => {
     console.log("calculate matching goal", values);
 
-    //calculate related liters/money
+    //all hell breaks loose if form is not validated
+    if (values.monthlyGoalType === GoalType.Liters) {
+      const goalLiters = parseInt(values.monthlyGoalAmount);
+
+      const priceForWater = goalLiters * waterLiterPrice;
+      const energyNeededToHeatOneLiter = getAverageHeatingForLiterOfWater(
+        data,
+        12
+      );
+      const priceHeating =
+        energyNeededToHeatOneLiter * goalLiters * electricityPrice;
+
+      console.log("TOTAL PRICE, euro", priceForWater + priceHeating);
+    }
+
+    if (values.monthlyGoalType === GoalType.Money) {
+    }
+
     //save to local state
   };
 
