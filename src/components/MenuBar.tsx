4331a573394add008bdc438af13aa0111e2b3d8a
Goda Plytaityte <goda.plytaityte@devbridge.com>
2021-11-20 23:05:29 +0200

    Change header color

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,37 +1,40 @@
 import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    IconButton,
-    Box,
-    Button,
-  } from "@chakra-ui/react"
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  Box,
+  Button,
+} from "@chakra-ui/react";
 
-import { BellIcon, CloseIcon, HamburgerIcon, SettingsIcon } from '@chakra-ui/icons'
+import {
+  BellIcon,
+  CloseIcon,
+  HamburgerIcon,
+  SettingsIcon,
+} from "@chakra-ui/icons";
+import { mediumGrayColor } from "../theme/colors";
 
 export const MenuBar = () => {
-return (<div>
-    <Box bg="gray" w="100%" p={4} color="white" rounded="md">
-    <Menu>
-    <MenuButton
-      as={IconButton}
-      aria-label="Options"
-      icon={<HamburgerIcon />}
-      variant="outline"
-    />
-    <MenuList bg="gray">
-      <MenuItem icon={<SettingsIcon />}>
-        Settings
-      </MenuItem>
-      <MenuItem icon={<CloseIcon />}>
-        Log Out
-      </MenuItem>
-    </MenuList>
-    <Button bg="red" width="40px">
-        <BellIcon/>
-    </Button>
-    FlOwless
-  </Menu>
-  </Box></div>)
+  return (
+    <Box bg={mediumGrayColor} w="100%" p={4} color="white">
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          aria-label="Options"
+          icon={<HamburgerIcon />}
+          variant="outline"
+        />
+        <MenuList bg="gray">
+          <MenuItem icon={<SettingsIcon />}>Settings</MenuItem>
+          <MenuItem icon={<CloseIcon />}>Log Out</MenuItem>
+        </MenuList>
+        <Button bg="red" width="40px">
+          <BellIcon />
+        </Button>
+        FlOwless
+      </Menu>
+    </Box>
+  );
 };
